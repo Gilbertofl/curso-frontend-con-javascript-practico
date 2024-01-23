b739edcd4acdfa1d3e5dfa914e16209289ee747a
Gilberto Farrera López <gilberto.programacion@gmail.com>
2024-01-23 01:39:42 -0600

    Avances de curso practico

---
+++ main.js
@@ -0,0 +1,105 @@
+const menuEmail = document.querySelector('.navbar-email');
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+const buguerMenu = document.querySelector('.menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuEmail.addEventListener('click', toggleDesktopMenu);
+buguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleCarroAside);
+
+function toggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+        if(!isAsideClosed) {
+            aside.classList.add('inactive');
+        }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarroAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopClosed = desktopMenu.classList.contains('inactive');
+    
+        if(!isMobileMenuClosed) {
+            mobileMenu.classList.add('inactive');
+        }
+        if(!isDesktopClosed) {
+            desktopMenu.classList.add('inactive');
+        }
+    aside.classList.toggle('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Licuadora',
+    price: 213,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pan',
+    price: 123123,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pan',
+    price: 123123,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+//La creacion de una funcion para encapsular el funcionamiento de la
+//array, ayuda mucho y para hacer funcionarla, hacemos referencia
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    
+        // maquetacion de html con javascript
+        productInfoDiv.append(productPrice, productName);
+    
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.append(productInfoDiv, productInfoFigure);
+    
+        productCard.append(productImg, productInfo)
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+//llamamos la funcion y le indicamos el sistema de array que va a redenrizar
+renderProducts(productList);
